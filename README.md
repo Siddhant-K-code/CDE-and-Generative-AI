bec99477e10d88b6edb07782effa9ee9a967019f
Siddhant Khare <Siddhantkhare2694@gmail.com>
2023-11-24 06:01:54 +0000

    Initial commit :rocket:

---
+++ README.md
@@ -0,0 +1,27 @@
+# 🤖 Coding Assistant
+
+## Overview
+
+Coding Assistant is a demonstrative web application showcasing how to effortlessly build Generative AI tools using Gitpod's Cloud development environment. This project exemplifies the ease of developing and running resource-intensive AI applications in the cloud, freeing developers from the constraints of local machine memory and performance issues. It's particularly designed for developers who seek assistance in understanding, optimizing, and commenting their GoLang code, employing OpenAI's language models for insightful code interactions.
+
+## Features
+
+- **Code Explanation**: Submit GoLang code to receive a simplified, bullet-point explanation of what the code does.
+- **Code Optimization**: Upload your GoLang code to get an optimized version, focusing on readability and performance while adhering to PEP8 standards.
+- **Code Commenting**: Enhance your GoLang code with comments that follow PEP8 standards, improving readability and maintainability.
+
+## Technology Stack
+
+- **Streamlit:** For the web interface.
+- **OpenAI API:** Powers the code processing and language model interactions.
+- **Python:** The primary language for backend development.
+- **Gitpod:** Provides a cloud-based development environment, ensuring smooth developer experience and handling of AI models.
+
+## Usage
+
+To use Coding assistant, Click on the following and you are good to go 🚀
+
+[![Open in Gitpod](https://www.gitpod.io/svg/open-in-gitpod.svg)](https://gitpod.io/#https://github.com/Siddhant-K-code/CDE-and-Generative-AI)
+
+> **Note:**
+> **API Key:** Obtain an API key from [OpenAI](https://platform.openai.com/account/api-keys).
---
+++ main.py
@@ -0,0 +1,94 @@
+import streamlit as st
+from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from io import StringIO
+
+# ============================================================
+# Consts
+# ============================================================
+PROMPT_EXPLAIN_CODE = "Can you explain what this GoLang code does by providing a bullet list: \n"
+PROMPT_OPTIMIZE_CODE = "Can you optimize code for readability, without losing on perfomance. Please re-write the whole code with a cleaner and easier to read version. Make sure it adheres to PEP8 standards. Please do not add any header to your response just return the code so I can copy/paste it as is. \n"
+PROMPT_COMMENT_CODE = "Please re-write the whole code with comments according to PEP8 standard. Do not change the behaviour of the code, just add the proper comments. Do not over-comment for no reason. Please do not add any header to your response just return the code so I can copy/paste it as is. \n"
+
+# ============================================================
+# Functions
+# ============================================================
+def generate_response(prompt, openai_api_key=None):
+    if not openai_api_key:
+        st.sidebar.error("Please add your OpenAI API key to continue.")
+        return None
+
+    llm = OpenAI(temperature=0.7, max_tokens=2500, openai_api_key=openai_api_key)
+    return llm(prompt)
+
+# ============================================================
+# Streamlit UI
+# ============================================================
+st.title("🤖 Coding Assistant")
+
+if "uploaded_code" not in st.session_state:
+    st.session_state["uploaded_code"] = None
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", type="password")
+    st.selectbox("OpenAI Model", ["gpt-4", "gpt-3.5-turbo", "gpt-3.5-turbo-instruct", "davinci"], key="openai_model")
+    #"[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    uploaded_file = st.file_uploader("Upload a GoLang File", type=["go"])
+    if uploaded_file:
+        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+        uploaded_code = stringio.read()
+        st.session_state["uploaded_code"] = uploaded_code
+        st.session_state["new_code"] = None
+    st.markdown("---")
+
+with st.expander("Review your Uploaded Code", expanded=(uploaded_file is not None)):
+    if uploaded_file:
+        st.code(uploaded_code, language="go")
+    else:
+        st.warning("Please upload your code (.go file) via the side panel.")
+
+col1, _, col2, _, col3 = st.columns([2,1,2,1,2])
+main_response_view = st.empty()
+main_response_button = st.empty()
+
+with col1:
+    explain_code = st.button("Explain Code", key="explain_code", disabled=(st.session_state["uploaded_code"] is None), use_container_width=True)
+    if explain_code:
+        prompt = PROMPT_EXPLAIN_CODE + st.session_state["uploaded_code"]
+        main_response_view.info(generate_response(prompt, openai_api_key))
+with col2:
+    optimize_code = st.button("Optimize Code", key="optimize_code", disabled=(st.session_state["uploaded_code"] is None), use_container_width=True)
+    if optimize_code:
+        prompt = PROMPT_OPTIMIZE_CODE + st.session_state["uploaded_code"]
+        with st.sidebar:
+            with st.spinner("Buddy working hard..."):
+                st.session_state["new_code"] = generate_response(prompt, openai_api_key)
+                main_response_view.code(st.session_state["new_code"])
+                main_response_button.download_button(
+                        label="Download Optimized Code",
+                        data=st.session_state["new_code"],
+                        file_name="optimized_code.go",
+                        mime="text/plain",
+                        use_container_width=True)
+                st.success("Work completed! 🎉")
+
+with col3:
+    comment_code = st.button("Comment Code", key="comment_code", disabled=(st.session_state["uploaded_code"] is None), use_container_width=True)
+    if comment_code:
+        prompt = PROMPT_COMMENT_CODE + st.session_state["uploaded_code"]
+        with st.sidebar:
+            with st.spinner("Buddy working hard..."):
+                st.session_state["new_code"] = generate_response(prompt, openai_api_key)
+                main_response_view.code(st.session_state["new_code"])
+                main_response_button.download_button(
+                        label="Download Commented Code",
+                        data=st.session_state["new_code"],
+                        file_name="commented_code.go",
+                        mime="text/plain",
+                        use_container_width=True)
+                st.success("Work completed! 🎉")
